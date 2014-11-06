429d14ebd3bf84b09d2f694b862294909e32e642
jruaux <jruaux AT tibco DOT com>
2014-11-06 15:28:37 -0800

    added wildcard name matching

--- src/main/java/com/tibco/as/util/Utils.java
+++ src/main/java/com/tibco/as/util/Utils.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Method;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Stack;
 
 import com.tibco.as.space.ASCommon;
 import com.tibco.as.space.ASException;
@@ -21,6 +22,9 @@ import com.tibco.as.space.Space;
 import com.tibco.as.space.SpaceDef;
 
 public class Utils {
+
+	private final static char WILDCARD_SINGLE = '?';
+	private final static char WILDCARD_MULTIPLE = '*';
 
 	public static boolean hasMethod(Class<?> clazz, String name) {
 		for (Method method : clazz.getMethods()) {
@@ -140,23 +144,8 @@ public class Utils {
 		return metaspace.getSpace(spaceName, distributionRole);
 	}
 
-	public static void copy(InputStream inp, OutputStream out)
-			throws IOException {
-		byte[] buff = new byte[4096];
-		int count;
-		while ((count = inp.read(buff)) != -1) {
-			if (count > 0) {
-				out.write(buff, 0, count);
-			}
-		}
-	}
-
 	public static File copy(String resource, File dir) throws IOException {
 		return copy(resource, dir, resource);
-	}
-
-	public static InputStream getResourceAsStream(String resource) {
-		return ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
 	}
 
 	public static File createTempDirectory() throws IOException {
@@ -181,16 +170,23 @@ public class Utils {
 		return file;
 	}
 
-	public static void copyToFile(String resource, File destination)
+	public static void copyToFile(String resource, File file)
 			throws IOException {
-		OutputStream out = new FileOutputStream(destination);
+		OutputStream out = new FileOutputStream(file);
 		try {
-			InputStream in = getResourceAsStream(resource);
+			InputStream in = ClassLoader.getSystemClassLoader()
+					.getResourceAsStream(resource);
 			if (in == null) {
 				throw new FileNotFoundException(resource);
 			}
+			byte[] buff = new byte[4096];
+			int count;
 			try {
-				Utils.copy(in, out);
+				while ((count = in.read(buff)) != -1) {
+					if (count > 0) {
+						out.write(buff, 0, count);
+					}
+				}
 			} finally {
 				in.close();
 			}
@@ -205,6 +201,117 @@ public class Utils {
 			fieldNames.add(fieldDef.getName());
 		}
 		return fieldNames;
+	}
+
+	public static boolean matches(String name, String pattern,
+			boolean ignoreCase) {
+		if (name == null && pattern == null) {
+			return true;
+		}
+		if (name == null || pattern == null) {
+			return false;
+		}
+		String[] wcs = splitOnTokens(pattern);
+		boolean anyChars = false;
+		int textIdx = 0;
+		int wcsIdx = 0;
+		Stack<int[]> backtrack = new Stack<int[]>();
+		do {
+			if (backtrack.size() > 0) {
+				int[] array = backtrack.pop();
+				wcsIdx = array[0];
+				textIdx = array[1];
+				anyChars = true;
+			}
+			while (wcsIdx < wcs.length) {
+				if (wcs[wcsIdx].equals(String.valueOf(WILDCARD_SINGLE))) {
+					textIdx++;
+					if (textIdx > name.length()) {
+						break;
+					}
+					anyChars = false;
+				} else if (wcs[wcsIdx]
+						.equals(String.valueOf(WILDCARD_MULTIPLE))) {
+					anyChars = true;
+					if (wcsIdx == wcs.length - 1) {
+						textIdx = name.length();
+					}
+				} else {
+					if (anyChars) {
+						textIdx = checkIndexOf(name, textIdx, wcs[wcsIdx],
+								ignoreCase);
+						if (textIdx == -1) {
+							break;
+						}
+						int repeat = checkIndexOf(name, textIdx + 1,
+								wcs[wcsIdx], ignoreCase);
+						if (repeat >= 0) {
+							backtrack.push(new int[] { wcsIdx, repeat });
+						}
+					} else {
+						if (!name.regionMatches(ignoreCase, textIdx,
+								wcs[wcsIdx], 0, wcs[wcsIdx].length())) {
+							break;
+						}
+					}
+					textIdx += wcs[wcsIdx].length();
+					anyChars = false;
+				}
+
+				wcsIdx++;
+			}
+			if (wcsIdx == wcs.length && textIdx == name.length()) {
+				return true;
+			}
+
+		} while (backtrack.size() > 0);
+
+		return false;
+	}
+
+	private static String[] splitOnTokens(String text) {
+		if (text.indexOf(WILDCARD_SINGLE) == -1
+				&& text.indexOf(WILDCARD_MULTIPLE) == -1) {
+			return new String[] { text };
+		}
+		char[] array = text.toCharArray();
+		ArrayList<String> list = new ArrayList<String>();
+		StringBuilder buffer = new StringBuilder();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == WILDCARD_SINGLE || array[i] == WILDCARD_MULTIPLE) {
+				if (buffer.length() != 0) {
+					list.add(buffer.toString());
+					buffer.setLength(0);
+				}
+				if (array[i] == WILDCARD_SINGLE) {
+					list.add(String.valueOf(WILDCARD_SINGLE));
+				} else if (list.isEmpty()
+						|| i > 0
+						&& list.get(list.size() - 1).equals(
+								String.valueOf(WILDCARD_MULTIPLE)) == false) {
+					list.add(String.valueOf(WILDCARD_MULTIPLE));
+				}
+			} else {
+				buffer.append(array[i]);
+			}
+		}
+		if (buffer.length() != 0) {
+			list.add(buffer.toString());
+		}
+		return list.toArray(new String[list.size()]);
+	}
+
+	private static int checkIndexOf(String str, int strStartIndex,
+			String search, boolean ignoreCase) {
+		int endIndex = str.length() - search.length();
+		if (endIndex >= strStartIndex) {
+			for (int i = strStartIndex; i <= endIndex; i++) {
+				if (str.regionMatches(ignoreCase, i, search, 0, search.length())) {
+					return i;
+				}
+			}
+		}
+		return -1;
 	}
 
 }
