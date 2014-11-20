a8fcd742d21d8f3f33bb8e8c005614e7a67ae9e6
jruaux <jruaux AT tibco DOT com>
2014-11-19 20:57:02 -0800

    added readString(inputStream) method

--- src/main/java/com/tibco/as/util/Utils.java
+++ src/main/java/com/tibco/as/util/Utils.java
@@ -1,10 +1,12 @@
 package com.tibco.as.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.text.MessageFormat;
@@ -312,6 +314,17 @@ public class Utils {
 			}
 		}
 		return -1;
+	}
+
+	public static String readString(InputStream in) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		StringBuilder out = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			out.append(line);
+			out.append("\n");
+		}
+		return out.toString();
 	}
 
 }
